1cd93404b73a7fb54fbb000878032ac8a62b21a3
kyle.hsieh@moxa.com <kyle.hsieh@moxa.com>
2018-06-21 15:35:07 +0800

    Add another test cases for lowercase file extension

--- ArtOfUnitTesting2ndEd.HandsOn/LogAn.UnitTests/LogAnalyzerTests.cs
+++ ArtOfUnitTesting2ndEd.HandsOn/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -9,9 +9,27 @@ namespace LogAn.UnitTests
         public void IsValidLogFileName_BadExtension_ReturnsFalse()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            bool result = analyzer
+                .IsValidLogFileName("filewithbadextension.foo");
             Assert.False(result);
-            
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer
+                .IsValidLogFileName("filewithgoodextension.slf");
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer
+                .IsValidLogFileName("filewithgoodextension.SLF");
+            Assert.True(result);
         }
     }
 }
