b0dbc16ddfd670dc5f539cae30c047a2ccf1c6d1
kyle.hsieh@moxa.com <kyle.hsieh@moxa.com>
2018-06-21 17:25:05 +0800

    Add test class for MemCalculator.

---
+++ ArtOfUnitTesting2ndEd.HandsOn/LogAn.UnitTests/MemCalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    class MemCalculatorTest
+    {
+        private static MemCalculator MakeMemCalculator()
+        {
+            return new MemCalculator();
+        }
+
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator mc = MakeMemCalculator();
+            var sum = mc.Sum();
+            Assert.AreEqual(0, sum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator mc = MakeMemCalculator();
+            mc.Add(1);
+            var sum = mc.Sum();
+            Assert.AreEqual(1, sum);
+        }
+    }
+}
